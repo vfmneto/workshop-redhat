7112de651e0fcf9d7a9e02f46679215287dbb5c7
vfmneto <vfmneto@gmail.com>
2018-04-12 20:38:29 +0000

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,7 +1,7 @@
 node('maven') {
     stage('build') {
         echo 'building app :)'
-        openshiftBuild(buildConfig: 'workshop-ocp', showBuildLogs: 'true')
+        openshiftBuild(buildConfig: 'workshop', showBuildLogs: 'true')
     }
     stage('verify') {
         echo 'dummy verification....'
